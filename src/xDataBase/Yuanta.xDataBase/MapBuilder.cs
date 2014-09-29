c169e7e42df21939dc9df5eb72736a8025e2c650
wbtsai <wbtsai@msn.com>
2014-09-29 17:29:41 +0800

    row mapping

---
+++ src/xDataBase/Yuanta.xDataBase/MapBuilder.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using Yuanta.xDataBase.RowMap;
+using System.Collections;
+
+namespace Yuanta.xDataBase
+{
+    public static class MapBuilder<TResult> where TResult:new()
+    {
+        public static MapBuilderContext<TResult> MapAll()
+        {
+            MapBuilderContext<TResult> context = new MapBuilderContext<TResult>();
+
+            var properties =
+                from property in typeof(TResult).GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                where IsAutoMappableProperty(property)
+                select property;
+
+            foreach (var property in properties)
+            {
+                context = context.Map(property);
+            }
+            return context;
+        }
+
+        private static bool IsAutoMappableProperty(PropertyInfo property)
+        {
+            return property.CanWrite
+              && property.GetIndexParameters().Length == 0
+              && !IsCollectionType(property.PropertyType)
+            ;
+        }
+
+        private static bool IsCollectionType(Type type)
+        {
+            // string implements IEnumerable, but for our purposes we don't consider it a collection.
+            if (type == typeof(string)) return false;
+
+            var interfaces = from inf in type.GetInterfaces()
+                             where inf == typeof(IEnumerable) ||
+                                 (inf.IsGenericType && inf.GetGenericTypeDefinition() == typeof(IEnumerable<>))
+                             select inf;
+            return interfaces.Count() != 0;
+        }
+        
+        private class MapBuilderContext<TResult>
+        {
+            public Dictionary<PropertyInfo, PropertyMapping> mapping;
+
+            public MapBuilderContext()
+            {
+                this.mapping = new Dictionary<PropertyInfo, PropertyMapping>(); 
+            }
+
+            public 
+            
+        }
+    }
+}
---
+++ src/xDataBase/Yuanta.xDataBase/RowMap/PropertyMapping.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Data;
+using Yuanta.xDataBase.RowMap;
+
+namespace Yuanta.xDataBase.RowMap
+{
+    public class PropertyMapping
+    {
+        public PropertyMapping(PropertyInfo propertyInfo)
+        {
+            this.Property = propertyInfo;
+            //確定是否有設ColumnName
+            ColumnNameAttribute[] attrs = (ColumnNameAttribute[])this.Property.GetCustomAttributes(typeof(ColumnNameAttribute));
+            if (attrs == null)
+            {
+                this.Name = this.Property.Name;
+            }
+            else 
+            {
+                this.Name = attrs[0].ColumnName;
+            }
+            
+        }
+        public PropertyInfo Property { get; private set; }
+
+        public string Name { get; private set; }
+
+        public object GetPropertyValue(DataRow row)
+        {
+            return row[this.Name];
+        }
+
+        public void Map(object instance, DataRow row)
+        {
+            object convertedValue = GetPropertyValue(row);
+
+            SetValue(instance, convertedValue);
+        }
+
+        protected void SetValue(object instance, object value)
+        {
+            Property.SetValue(instance, value, new object[0]);
+        }
+    }
+}
