665d136e6bf648a8213a3a724106d58948ad158c
binbin <wbtsai@msn.com>
2014-09-30 22:17:42 +0800

    merge

--- src/xDataBase/Yuanta.xDataBase/DataBaseExtension.cs
+++ src/xDataBase/Yuanta.xDataBase/DataBaseExtension.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace Yuanta.xDataBase
 {
-	public class DataBaseExtension
+	public static class DataBaseExtension
 	{
-		public DataBaseExtension ()
+		public static List<T> ToResultT<T>(this DataTable dt)
 		{
+			return MapByRow<T> (dt).ToList ();
 		}
+
+		private static IEnumerable<T> MapByRow<T>(DataTable dt)
+		{
+			foreach (DataRow row in dt.rows) {
+			
+				yield return MappingRow (row);
+			}
+		}
+
+
+
+
 	}
 }
 
