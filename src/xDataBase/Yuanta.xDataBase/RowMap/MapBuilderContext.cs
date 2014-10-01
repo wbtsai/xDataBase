0fc2d52dfe2619a7e4794a6babdd608cb3825729
wbtsai <wbtsai@msn.com>
2014-10-02 07:57:26 +0800

    Add MapBuilderContext

---
+++ src/xDataBase/Yuanta.xDataBase/RowMap/MapBuilderContext.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Data;
+using System.Collections;
+using System.Linq.Expressions;
+
+namespace Yuanta.xDataBase.RowMap
+{
+    public class MapBuilderContext<TResult>
+    {
+        private Dictionary<PropertyInfo, PropertyMapping> propertyMapping;
+
+        private Func<DataRow, TResult> mapper;
+
+        public MapBuilderContext()
+        {
+            propertyMapping = new Dictionary<PropertyInfo, PropertyMapping>();
+
+            var properties =
+                from property in typeof(TResult).GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                where IsAutoMappableProperty(property)
+                select property;
+            
+            foreach (var property in properties)
+            {
+                propertyMapping.Add(property, new PropertyMapping(property));
+            }
+
+            //Get Reflection Structure
+            ReflectionRowMapper();
+        }
+
+        private static bool IsAutoMappableProperty(PropertyInfo property)
+        {
+            return property.CanWrite
+              && property.GetIndexParameters().Length == 0
+              && !IsCollectionType(property.PropertyType)
+            ;
+        }
+
+        private static bool IsCollectionType(Type type)
+        {
+            // string implements IEnumerable, but for our purposes we don't consider it a collection.
+            if (type == typeof(string)) return false;
+
+            var interfaces = from inf in type.GetInterfaces()
+                             where inf == typeof(IEnumerable) ||
+                                 (inf.IsGenericType && inf.GetGenericTypeDefinition() == typeof(IEnumerable<>))
+                             select inf;
+            return interfaces.Count() != 0;
+        }
+        
+        private void ReflectionRowMapper()
+        {
+            if (propertyMapping == null) throw new ArgumentNullException("propertyMappings");
+
+                var parameter = Expression.Parameter(typeof(DataRow), "dr");
+                var newExpr = Expression.New(typeof(TResult));
+                var ConvertValue =GetMethodInfo<PropertyMapping>(pm => pm.GetPropertyValue(null));
+                var bindings =
+                    propertyMapping.Select(kvp => (MemberBinding)
+                        Expression.Bind(
+                            kvp.Key,
+                            Expression.Convert(
+                                Expression.Call(Expression.Constant(kvp.Value), ConvertValue, new Expression[] { parameter }),
+                                kvp.Key.PropertyType)));
+
+                var body = Expression.MemberInit(newExpr, bindings);
+                var lambda = Expression.Lambda<Func<DataRow,TResult>>(body, parameter);
+                this.mapper = lambda.Compile();
+            
+        }
+
+        public MethodInfo GetMethodInfo<T>(Expression<Action<T>> expression)
+        {
+            return GetMethodInfo((LambdaExpression)expression);
+        }
+
+        private MethodInfo GetMethodInfo(LambdaExpression lambda)
+        {
+            var call = (MethodCallExpression)lambda.Body;
+            return call.Method;
+        }
+
+        public TResult MapRow(DataRow row)
+        {
+            return mapper(row);
+        }
+    }
+}
