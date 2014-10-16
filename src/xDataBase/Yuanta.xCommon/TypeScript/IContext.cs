4f939853952ce334a14b105fd9200f93295b51b8
wbtsai <wbtsai@msn.com>
2014-10-16 15:52:07 +0800

    Fix Parser Function

---
+++ src/xDataBase/Yuanta.xCommon/TypeScript/IContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yuanta.xCommon.TypeScript
+{
+    public interface IContext<A,U>
+    {
+        bool success { get; set; }
+
+        U UserState { get; set; }
+
+        Func<Parser<A, U>, U> Parse { get; set; }
+    }
+}
--- src/xDataBase/Yuanta.xCommon/TypeScript/Parser.cs
+++ src/xDataBase/Yuanta.xCommon/TypeScript/Parser.cs
@@ -8,12 +8,36 @@ namespace Yuanta.xCommon.TypeScript
 {
     public class Parser
     {
-
-
-        public Parser()
-        { 
-        
+        public Reply<A,U> Parse<A,U>(Parser<A,U> parser,State<U> state )
+        {
+            return parser.RunParser(state);
         }
+
+        public Parser<A, U> Choice<A,U>(List<Parser<A, U>> ps)
+        {
+            Func<State<U>, Reply<A, U>> choiceParser = state =>
+            {
+               List<Reply<A,U>> sts =new List<Reply<A,U>>();
+                
+                foreach(var s in ps)
+                {
+                    Reply<A,U> st=this.Parse(s,state);
+
+                    if(st.Success||st.State.Position!=state.Position)
+                    {
+                        return st;
+                    }
+
+                    sts.Add(st);
+                }
+
+                return Error(state, () => { return "one of string error"; });
+                
+            };
+
+            return new Parser<A, U>(choiceParser);
+        }
+
 
         public State<U> State<U>(string source, U userState, int position = 0)
         {
@@ -33,10 +57,11 @@ namespace Yuanta.xCommon.TypeScript
 
     public class Parser<A,U>
     {
-        public Parser(Func<State<U>, Reply<A, U>> runparser)
+       public Func<State<U>, Reply<A, U>> RunParser{get;set;}
+       public Parser(Func<State<U>, Reply<A, U>> runparser)
         {
             this.RunParser = runparser;
         }
-        public Func<State<U>, Reply<A, U>> RunParser{ get; set; }
+       
     }
 }
