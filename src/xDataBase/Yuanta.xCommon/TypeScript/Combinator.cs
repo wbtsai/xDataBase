ecb0fd1288df3000612d09273857ba7c4fe43eff
wbtsai <wbtsai@msn.com>
2014-10-21 18:43:05 +0800

    Add comment

--- src/xDataBase/Yuanta.xCommon/TypeScript/Combinator.cs
+++ src/xDataBase/Yuanta.xCommon/TypeScript/Combinator.cs
@@ -34,5 +34,46 @@ namespace Yuanta.xCommon.TypeScript
             
             return new Parser<A>(choiceParser);
         }
+
+        public Parser<List<T>> Repeat<T>(int min,int max,Parser<T> parser)
+        {
+            Func<State,List<T>> repeatParser=state=>{
+                List<T> xs=new List<T>();
+                
+                Reply<T> st=new Parser().OK<T>(state,default(T));
+
+                for(int i=0;i<max;i++)
+                {
+                    var st1=new Parser().Parse<T>(parser,st.State);
+
+                    if(st1.Success)
+                    {
+                        if(st1.State.Position == st.State.Position && max ==int.MaxValue)
+                        {
+                            throw new Exception("many combinator is applied to a parser that accepts an empty string.");
+                        }
+                        else
+                        {
+                            st=st1;
+                            xs.Add(st.Value);
+                        }
+                    }
+                    else if(st.State.Position < st1.State.Position)
+                    {
+                        return st1;
+                    }else if(i < min){
+                        return st1;
+                    }else{
+                        break;
+                    }
+                }
+
+                return new Parser().OK<T>(st.State, xs);
+            };
+            return new Parser<T>(repeatParser);
+        }
+        
+        
+    }
     }
 }
